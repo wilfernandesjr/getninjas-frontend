e52afda0a77662aefa9509a1241d07b01b7b88a0
wilfernandesjr <wilmar.wfsj@gmail.com>
2019-09-03 02:26:12 -0300

    Add SelectItem component

---
+++ src/components/SelectItem/index.js
@@ -0,0 +1,52 @@
+import {
+  renderElement,
+  toJSON
+} from '../../services/utils.js'
+
+import { Component } from '../Component'
+
+class SelectItem extends Component {
+  connectedCallback () {
+    this.props = {
+      key: this.getAttribute('key'),
+      field: toJSON(this.getAttribute('field')),
+      message: this.getAttribute('message')
+    }
+    this.render()
+
+    const $select = this.querySelector('select')
+    $select.addEventListener('focusout', this.handleChange.bind(this))
+  }
+
+  handleChange (e) {
+    const $select = e.currentTarget
+    const $selectWrapper = this.querySelector('.selectItem')
+    if (!$select.value && $select.getAttribute('required')) {
+      return $selectWrapper.classList.add('selectItem--error')
+    }
+    $selectWrapper.classList.remove('selectItem--error')
+  }
+
+  render () {
+    const { key, field, message } = this.props
+    renderElement(this, `
+      <div class="selectItem">
+        ${field.label ? `<label for="${key}">${field.label}</label>` : ''}
+        <select
+          id="${key}"
+          class="selectItem__input"
+          ${field.required ? 'required="true"' : ''}
+        >
+          <option selected value="">${field.mask}</option>
+          ${Object.keys(field.values || []).map(value => `
+            <option value="${field.values[value]}">${field.values[value]}</option>
+          `).join('')}
+        </select>
+        <span class="selectItem__message">${message}</span>
+      </div>
+    `)
+  }
+}
+
+customElements.define('select-item', SelectItem)
+document.createElement('select-item')
